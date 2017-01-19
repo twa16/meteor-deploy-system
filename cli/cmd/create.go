318add22f3965d6df292ad56ca62caf85d5dbce3
Manuel Gauto <mgauto@mgenterprises.org>
2017-01-19 14:28:45 -0500

    Work on deployment create command for cli. Move test working data to ignored dir.

--- cli/cmd/create.go
+++ cli/cmd/create.go
@@ -24,22 +24,199 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"net/http"
+	"bytes"
+	"github.com/spf13/viper"
+	"mime/multipart"
+	"os"
+	"io"
+	"net/url"
+	"bufio"
+	"strings"
+	"github.com/k0kubun/pp"
+	"io/ioutil"
 )
 
 // createCmd represents the create command
 var createCmd = &cobra.Command{
-	Use:   "create",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "create [path to tarball] [path to settings.json]",
+	Short: "Create a deployment",
+	Long: `Creates a deployment on the MDS server`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("create called")
+
+		type ProjectData struct {
+			projectName string
+			tarballPath string
+			settingsPath string
+			envVars []string
+		}
+		var project ProjectData
+		//Check if the files have been passed as parameters
+		if len(args) > 0 {
+			if len(args) > 0 {
+				project.tarballPath = args[0]
+				if _, err := os.Stat(project.tarballPath); os.IsNotExist(err) {
+					fmt.Println("The specified project tarball does not exist")
+				}
+			}
+			if len(args) > 1 {
+				project.settingsPath = args[1]
+				if _, err := os.Stat(project.settingsPath); os.IsNotExist(err) {
+					fmt.Println("The specified settings file does not exist")
+				}
+			}
+		}
+
+		//Get input for other parameters
+		reader := bufio.NewReader(os.Stdin)
+
+		//Get file paths if needed
+		if project.tarballPath == "" {
+			fmt.Print("Path to Tarball: ")
+			project.tarballPath, _ = reader.ReadString('\n')
+			project.tarballPath = strings.TrimSpace(project.tarballPath)
+			if _, err := os.Stat(project.tarballPath); os.IsNotExist(err) {
+				fmt.Println("The specified project tarball does not exist")
+			}
+		}
+		if project.settingsPath == "" {
+			fmt.Print("Path to Settings Json(optional): ")
+			project.settingsPath, _ = reader.ReadString('\n')
+			project.settingsPath = strings.TrimSpace(project.settingsPath)
+			if project.settingsPath != "" {
+				if _, err := os.Stat(project.settingsPath); os.IsNotExist(err) {
+					fmt.Println("The specified project tarball does not exist")
+				}
+			}
+		}
+
+		//Get Project Name
+		fmt.Print("Enter Project Name: ")
+		project.projectName, _ = reader.ReadString('\n')
+		project.projectName = strings.TrimSpace(project.projectName)
+
+		//Get Env Variables
+		fmt.Println("Please enter environmental variables as KEY=VALUE. If you are finished, enter 'done' as the value.")
+		//Loop until the user is done
+		for true {
+			fmt.Print("Enter EnvVar(KEY=VALUE): ")
+			envVar, _ := reader.ReadString('\n')
+			envVar = strings.TrimSpace(envVar)
+			//Check if the input is our escape word
+			if strings.ToLower(envVar) == "done" {
+				break
+			}
+			//Ignore blanks
+			if envVar != "" {
+				//Otherwise, add it to our array
+				project.envVars = append(project.envVars, envVar)
+				//Print what has been entered
+				fmt.Println("Current Env Vars:")
+				for _, val := range(project.envVars) {
+					fmt.Println("  "+val)
+				}
+				fmt.Print("\nEnter 'done' when finished.\n\n")
+			}
+		}
+
+		pp.Println(project)
+		fmt.Print("Do you wish to create this project. Please type in 'yes' or 'no': ")
+		input, _ := reader.ReadString('\n')
+		if strings.TrimSpace(input) == "yes" {
+			fmt.Print("Processing Settings File ")
+			settingBytes, err := ioutil.ReadFile(project.settingsPath)
+			if err != nil {
+				fmt.Println("     FAIL!")
+				fmt.Println("Error: "+err.Error())
+				return
+			}
+			fmt.Println("     OK.")
+
+			createDeployment(project.tarballPath, project.projectName, string(settingBytes), project.envVars)
+		}
 	},
+}
+
+func createDeployment(pathToTarball string, projectName string, settings string, envVars []string) {
+	//Let's build the url
+	hostname := viper.GetString("ServerHostname")
+	isSecure := viper.GetBool("UseHTTPS")
+	authToken := viper.GetString("AuthenticationToken")
+
+	urlString := hostname + "/login"
+	//Check if the connection should be secure and prepend the proper protocol
+	if isSecure {
+		urlString = "https://" + urlString
+	} else {
+		urlString = "http://" + urlString
+	}
+
+	//Parse URL
+	reqURL, err := url.Parse(urlString)
+	if err != nil {
+		fmt.Println("Invalid URL Specified!")
+		return
+	}
+
+	//Store projectname in URL
+	reqURL.Query().Add("projectname", projectName)
+	//Store the file and settings as byte buffer for body
+	data := createForm(settings, pathToTarball, envVars)
+
+	//Create the client
+	client := &http.Client{}
+	r, _ := http.NewRequest("POST", urlString, data)
+	r.Header.Add("Content-Type", "multipart/form-data")
+	r.Header.Add("X-Auth-Token", authToken)
+	resp, err := client.Do(r)
+	if err != nil {
+		fmt.Println("Failed to create deployment")
+		panic(err)
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	fmt.Println(buf.String())
+}
+
+func createForm(settings string, file string, envVars []string) (*bytes.Buffer) {
+	// Prepare a form that you will submit to that URL.
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	// Add your image file
+	f, err := os.Open(file)
+	if err != nil {
+		return nil
+	}
+	defer f.Close()
+	fw, err := w.CreateFormFile("image", file)
+	if err != nil {
+		return nil
+	}
+	if _, err = io.Copy(fw, f); err != nil {
+		return nil
+	}
+	// Add the settings
+	if fw, err = w.CreateFormField("settings"); err != nil {
+		return nil
+	}
+	if _, err = fw.Write([]byte(settings)); err != nil {
+		return nil
+	}
+
+	//Add any env vars
+	for _, envVar := range(envVars) {
+		if fw, err = w.CreateFormField("Env-Var"); err != nil {
+			return nil
+		}
+		if _, err = fw.Write([]byte(envVar)); err != nil {
+			return nil
+		}
+	}
+
+	// Don't forget to close the multipart writer.
+	// If you don't close it, your request will be missing the terminating boundary.
+	w.Close()
+	return &b
 }
 
 func init() {
--- cli/cmd/deployment.go
+++ cli/cmd/deployment.go
@@ -19,16 +19,11 @@ import "github.com/spf13/cobra"
 // deploymentCmd represents the deployment command
 var deploymentCmd = &cobra.Command{
 	Use:   "deployment",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Commands for managing deployments",
+	Long: `The deployment command is the parent command for the various deployment management commands.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-
+		cmd.Help()
 	},
 }
 
